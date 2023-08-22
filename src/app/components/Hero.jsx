5dd4f5873a73a295df347015fbdeafd0f302701d
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2023-08-22 16:16:05 +0530

    minor changes

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -15,14 +15,14 @@ const HeroPage = () => {
       id='home'
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-20'>
         <p className='text-xl font-montserrat text-coral-red'>
           Our Summer collections
         </p>
 
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
-            The New Arrival
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[80px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-15'>
+          Newly Arrived
           </span>
           <br />
           <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
