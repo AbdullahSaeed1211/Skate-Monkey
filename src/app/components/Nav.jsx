46a8e0b4d044cdb432959bf534aff314b457d789
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2023-08-23 10:50:13 +0530

    teal accents

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -24,27 +24,18 @@ const Nav = () => {
             Skate Monkey
           </span>
         </Link>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          {navLinks.map((item) => (
-            <li key={item.label}>
-              <Link
-                href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray hover:underline hover:text-white">
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
         <div>
           <div
             className="hidden max-lg:block cursor-pointer"
-            onClick={toggleMenu}>
+            onClick={toggleMenu}
+          >
             <Image src={hamburger} alt="Hamburger" width={25} height={25} />
           </div>
           {menuOpen && (
             <div
               className="fixed top-0 left-0 h-full w-full bg-white flex flex-col items-center"
-              style={{ zIndex: 1000 }}>
+              style={{ zIndex: 1000 }}
+            >
               <div className="text-right p-4 ml-auto">
                 <button onClick={closeMenu}>
                   <Image src={close} alt="Close Icon" width={20} height={20} />
@@ -56,7 +47,9 @@ const Nav = () => {
                     <li key={item.label} className="py-4">
                       <Link
                         href={item.href}
-                        className="font-montserrat leading-normal text-2xl text-black hover:underline hover:text-purple-800">
+                        onClick={closeMenu} // Add this line to close the menu on link click
+                        className="font-montserrat leading-normal text-2xl text-black hover:underline hover:text-purple-800"
+                      >
                         {item.label}
                       </Link>
                     </li>
@@ -72,3 +65,4 @@ const Nav = () => {
 };
 
 export default Nav;
+
